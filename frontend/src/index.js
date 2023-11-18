d21ee1cba2efae916bd82bb41fbf6e0d4ddf2efc
ZekkenCL <benito.munoz@alumnos.ucn.cl>
2023-11-18 16:50:48 -0300

    move styles.css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importar Bootstrap aquí
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import './assets/images/styles.css'; // Importar estilos personalizados aquí
+import './assets/css/styles.css'; // Importar estilos personalizados aquí
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
